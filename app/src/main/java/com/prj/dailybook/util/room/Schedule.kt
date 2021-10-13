2ed98572daa7c30082243fb8bf6c1a1704c21cfa
MyeongSeong <ms1994s@naver.com>
2021-10-13 14:34:09 +0900

    스케줄 DB 생성

---
+++ app/src/main/java/com/prj/dailybook/util/room/Schedule.kt
@@ -0,0 +1,13 @@
+package com.prj.dailybook.util.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "schedule")
+data class Schedule(
+    @PrimaryKey(autoGenerate = true) val Id: Long,
+    @ColumnInfo val date: String,
+    @ColumnInfo val contents: String,
+    @ColumnInfo val Yn: String
+)
